14e2a227070de4b6b8c25730109011b21dedd112
Loren Cipriano <loren25cipriano@gmail.com>
2021-04-07 15:19:51 -0700

    commit tree 3cf56acab6c0a57ccd9a9306d7d072f8a168f86e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :non_clickbait
+
+
+    def non_clickbait
+        unless [/Won't Believe/i,/Secret/i,/Guess/i,/Top [0-9]+/i].any? {|i| self.title=~ i }
+            errors.add(:title, "True Facts")
+        end
+    end
 end
